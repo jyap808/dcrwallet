a6bbb52499321aaf0731c4c96bb92b2a386bba47
Josh Rickmar <jrick@companyzero.com>
2026-08-18 12:02:06 +0000

    Bump version to 2.1.6+release.local.

--- version/version.go
+++ version/version.go
@@ -1,5 +1,5 @@
 // Copyright (c) 2013-2014 The btcsuite developers
-// Copyright (c) 2015-2025 The Decred developers
+// Copyright (c) 2015-2026 The Decred developers
 // Use of this source code is governed by an ISC
 // license that can be found in the LICENSE file.
 
@@ -20,7 +20,7 @@ const semverAlphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrst
 const (
 	Major = 2
 	Minor = 1
-	Patch = 5
+	Patch = 6
 )
 
 // Integer is an integer encoding of the major.minor.patch version.
